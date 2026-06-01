07d495622593647369dcdefa003a816bfec926a7
Madelyn Gueta <madelyngueta306@gmail.com>
2026-06-01 19:41:26 +0800

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,10 @@
+def test_calorie_known_food():
+        Pass
+def test_calorie_unknown_food():
+        Pass
+def test_add_meal():
+        Pass
+def test_clear_all(): 
+        Pass
+def test_storage_file_exists():
+        Pass
